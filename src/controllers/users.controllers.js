5077410c58246ec3b521923e0a79d83b293fb1dc
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-11-15 09:46:08 -0500

    API MODIFICADA

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -6,80 +6,63 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 const JWT_SECRET = process.env.JWT_SECRET
-const bcryptSalt = bcrypt.genSaltSync(10)
+const BCRYPT_SALT_ROUNDS = 10
 
-// TODO: /profile
 export const getUser = async (req, res) => {
   try {
-    const token1 = req.headers.authorization
-    const token = token1.split(' ')[1]
-    if (token) {
-      jwt.verify(token, JWT_SECRET, { sameSite: 'none', secure: 'true' }, (err, userData) => {
-        if (err) throw err
-        res.json(userData)
-      })
+    const token = req.headers.authorization?.split(' ')[1]
+    if (!token) {
+      throw new Error('No Token')
     }
+    const userData = jwt.verify(token, JWT_SECRET, { sameSite: 'none', secure: 'true' })
+    res.json(userData)
   } catch (error) {
-    res.status(401).json('No Token')
+    res.status(401).json(error.message)
   }
 }
 
-// TODO: /login
 export const getLogin = async (req, res) => {
-  const { user, password } = req.body
-
-  const [result] = await connectMysql.query(`SELECT username, password, nombres, apellidos FROM login WHERE username = '${user}'`)
-
-  if (result.length > 0) {
-    const userData = result.find((i) => i)
-    const { username, password: passDb, id, nombres, apellidos } = userData
-    const passOk = bcrypt.compareSync(password, passDb)
-
-    if (passOk) {
-      jwt.sign({ id, username, nombres, apellidos }, JWT_SECRET, {}, (err, token) => {
-        if (err) throw err
-        res.cookie('token', token, { sameSite: 'none', secure: 'true' }).status(200).json({ id, username, nombres, apellidos, token })
-      })
-    } else {
-      res.status(401).json({
-        error: 'Contraseña Incorrecta',
-        detalle: 'La clave del usuario no coinciden'
-      })
+  try {
+    const { user, password } = req.body
+    const [result] = await connectMysql.query('SELECT id, nombre, apellidos, correo, username, password, proceso FROM login_chat_v1 WHERE username = ?', [user])
+    if (result.length === 0) {
+      throw new Error(`El usuario: ${user}, No existe en el sistema.`)
     }
-  } else {
-    res.status(404).json({
-      error: 'Usuario No Registrado',
-      detalle: `El usuario: ${user}, No existe en el sistema.`
-    })
+    const userData = result[0]
+    const { id, nombre, apellidos, correo, username, password: hashedPassword, proceso } = userData
+    const passwordMatches = await bcrypt.compare(password, hashedPassword)
+    if (!passwordMatches) {
+      throw new Error('Contraseña Incorrecta')
+    }
+    const token = jwt.sign({ id, username, nombre, apellidos, correo, proceso }, JWT_SECRET)
+    res.cookie('token', token, { sameSite: 'none', secure: 'true' }).status(200).json({ id, username, nombre, apellidos, correo, proceso })
+  } catch (error) {
+    res.status(401).json({ error: error.message })
   }
 }
 
-// TODO: /register
 export const createUser = async (req, res) => {
-  const { names, lastNames, document } = req.body
-
   try {
-    const [result] = await connectMysql.query(`SELECT * FROM login WHERE documento = '${document}'`)
-
-    if (!result.length > 0) {
-      const username = `CP${document}`
-      const pass = `CP${document.slice(-3)}`
-      const hashedPassword = bcrypt.hashSync(pass, bcryptSalt)
-
-      const [UserCreado] = await connectMysql.query(`INSERT INTO login (username, password, nombres, apellidos, documento) 
-      VALUES ('${username}', '${hashedPassword}', '${names} ', '${lastNames}', '${document}')`)
-
-      if (UserCreado.affectedRows === 1) {
-        res.status(201).json({ message: 'Usuario Registrado Correctamente' })
-      } else {
-        res.status(500).json({ message: 'Usuario No Creado' })
-      }
-    } else {
+    const { nombre, apellidos, documento, telefono, correo } = req.body
+    const [result] = await connectMysql.query('SELECT * FROM login_chat_v1 WHERE documento = ?', [documento])
+    if (result.length > 0) {
       res.status(200).json({ message: 'Usuario Ya Se Encuentra Registrado' })
+      return
+    }
+    const username = `CP${documento}`
+    const password = `CP${documento.slice(-3)}`
+    const hashedPassword = await bcrypt.hash(password, BCRYPT_SALT_ROUNDS)
+    const proceso = 0
+    const [UserCreado] = await connectMysql.query(
+      'INSERT INTO login_chat_v1 (id, nombre, apellidos, documento, telefono, correo, username, password, estado, proceso) VALUES (UUID_TO_BIN(UUID()), ?, ?, ?, ?, ?, ?, ?, 1, ?)',
+      [nombre, apellidos, documento, telefono, correo, username, hashedPassword, proceso]
+    )
+    if (UserCreado.affectedRows === 1) {
+      res.status(201).json({ message: 'Usuario Registrado Correctamente' })
+    } else {
+      throw new Error('Usuario No Creado')
     }
   } catch (error) {
-    res.status(409).json({
-      error
-    })
+    res.status(409).json({ error: error.message })
   }
 }
