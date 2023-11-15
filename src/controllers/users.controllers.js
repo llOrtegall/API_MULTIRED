a5954d100ce1a3d1256f068c1958b1fc97335747
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-11-15 15:34:26 -0500

    funciona por el momento

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -24,18 +24,18 @@ export const getUser = async (req, res) => {
 export const getLogin = async (req, res) => {
   try {
     const { user, password } = req.body
-    const [result] = await connectMysql.query('SELECT id, nombre, apellidos, correo, username, password, proceso FROM login_chat_v1 WHERE username = ?', [user])
+    const [result] = await connectMysql.query('SELECT id, nombres, apellidos, correo, username, password, proceso FROM login_chat WHERE username = ?', [user])
     if (result.length === 0) {
-      throw new Error(`El usuario: ${user}, No existe en el sistema.`)
+      throw new Error('Credenciales inválidas')
     }
     const userData = result[0]
-    const { id, nombre, apellidos, correo, username, password: hashedPassword, proceso } = userData
+    const { id, nombres, apellidos, correo, username, password: hashedPassword, proceso } = userData
     const passwordMatches = await bcrypt.compare(password, hashedPassword)
     if (!passwordMatches) {
-      throw new Error('Contraseña Incorrecta')
+      throw new Error('Credenciales inválidas')
     }
-    const token = jwt.sign({ id, username, nombre, apellidos, correo, proceso }, JWT_SECRET)
-    res.cookie('token', token, { sameSite: 'none', secure: 'true' }).status(200).json({ id, username, nombre, apellidos, correo, proceso, token })
+    const token = jwt.sign({ id, username, nombres, apellidos, correo, proceso }, JWT_SECRET)
+    res.cookie('token', token, { sameSite: 'none', secure: 'true' }).status(200).json({ id, username, nombres, apellidos, correo, proceso, token })
   } catch (error) {
     res.status(401).json({ error: error.message })
   }
@@ -44,7 +44,7 @@ export const getLogin = async (req, res) => {
 export const createUser = async (req, res) => {
   try {
     const { nombres, apellidos, documento, telefono, correo, proceso } = req.body
-    const [result] = await connectMysql.query('SELECT * FROM login_chat_v1 WHERE documento = ?', [documento])
+    const [result] = await connectMysql.query('SELECT * FROM login_chat WHERE documento = ?', [documento])
     if (result.length > 0) {
       res.status(200).json({ message: 'Usuario Ya Se Encuentra Registrado' })
       return
@@ -53,8 +53,9 @@ export const createUser = async (req, res) => {
     const password = `CP${documento.slice(-3)}`
     const hashedPassword = await bcrypt.hash(password, BCRYPT_SALT_ROUNDS)
     const [UserCreado] = await connectMysql.query(
-      `INSERT INTO login_chat_v1 (nombres, apellidos, documento, telefono, correo, username, password, estado, empresa, proceso, rol) 
-        VALUES ('${nombres}', '${apellidos}', ${documento}, ${telefono}, '${correo}', '${username}', '${hashedPassword}', TRUE, 1, ${proceso}, 'ninguno');`
+      `INSERT INTO login_chat (nombres, apellidos, documento, telefono, correo, username, password, estado, empresa, proceso, rol) 
+        VALUES (?, ?, ?, ?, ?, ?, ?, TRUE, 1, ?, 'ninguno');`,
+      [nombres, apellidos, documento, telefono, correo, username, hashedPassword, proceso]
     )
     if (UserCreado.affectedRows === 1) {
       res.status(201).json({ message: 'Usuario Registrado Correctamente' })
@@ -72,9 +73,9 @@ export const changePassword = async (req, res) => {
     const { username, oldPassword, newPassword, confirmPassword } = req.body
 
     // Fetch the user
-    const [users] = await connectMysql.query('SELECT * FROM login_chat_v1 WHERE username = ?', [username])
+    const [users] = await connectMysql.query('SELECT * FROM login_chat WHERE username = ?', [username])
     if (users.length === 0) {
-      throw new Error('User not found')
+      throw new Error('Credenciales inválidas')
     }
 
     const user = users[0]
@@ -82,12 +83,12 @@ export const changePassword = async (req, res) => {
     // Check the old password
     const passwordMatches = await bcrypt.compare(oldPassword, user.password)
     if (!passwordMatches) {
-      throw new Error('Old password is incorrect')
+      throw new Error('Credenciales inválidas')
     }
 
     // Validate the new password
     if (newPassword !== confirmPassword) {
-      throw new Error('New passwords do not match')
+      throw new Error('Las nuevas contraseñas no coinciden')
     }
 
     // Hash the new password
@@ -96,7 +97,7 @@ export const changePassword = async (req, res) => {
     // Update the password in the database
     await connectMysql.query('UPDATE login_chat_v1 SET password = ? WHERE username = ?', [hashedPassword, username])
 
-    res.status(200).json({ message: 'Password updated successfully' })
+    res.status(200).json({ message: 'Contraseña actualizada correctamente' })
   } catch (error) {
     res.status(400).json({ error: error.message })
   }
