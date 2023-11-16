f47c97662942b97ad8daceae5682bd00098850b3
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-11-16 08:33:04 -0500

    CAMBIOS OK

--- src/services/htmlCreatedUser.js
+++ src/services/htmlCreatedUser.js
@@ -1,76 +1,93 @@
 export function htmlCreatedUser ({ nombre, cedula, telefono }) {
+  function lastThreeLetters (str) {
+    return str.slice(-3)
+  }
+
+  const redCedula = lastThreeLetters(cedula.toString())
+  const redTelefono = lastThreeLetters(telefono.toString())
+
   return (
-    `
+        `
     <!DOCTYPE html>
     <html lang="en">
-    
     <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-    </head>
-    
-    <style>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Información de Usuario Creada</title>
+      <style>
         body {
-            margin: 0;
-            padding: 20px 40px;
+          font-family: Arial, sans-serif;
+          margin: 0;
+          padding: 0;
+          background-color: #f4f4f4;
+        }
+    
+        main {
+          max-width: 800px;
+          margin: 0 auto;
+          background-color: #fff;
+          padding: 20px;
+          border-radius: 5px;
+          box-shadow: 0px 0px 10px rgba(0,0,0,0.1);
         }
     
         h1 {
-            text-align: center;
+          color: #444;
         }
     
-        .art1 {
-            display: flex;
-            justify-content: space-around;
-            font-size: 1.3rem;
-            font-weight: bold;
+        article {
+          margin-top: 20px;
+          font-size: 1.1rem;
         }
     
-        .art2 {
-            padding-top: 20px;
-            font-size: 1.1rem;
-            text-align: justify;
-        }
-    
-        .art3 {
-            margin-top: 20px;
-            font-size: 1.1rem;
+        p {
+          line-height: 1.6;
+          color: #666;
         }
     
         span {
-            font-weight: bold;
+          font-weight: bold;
         }
-    </style>
     
+        .art1{
+          display: flex;
+          width: 100%;
+          flex-direction: row;
+          justify-content: space-around;
+        }
+    
+        .art2{
+          text-align: justify;
+        }
+    
+        .pfoter {
+          font-size: 1.2rem;
+          font-weight: bold;
+        }
+      </style>
+    </head>
     <body>
-        <h1>Información De Usuario Creada</h1>
+      <main>
+        <h2>Información de Usuario Recibida y Creada</h2>
     
         <article class="art1">
-            <p>Nombre ${nombre}</p>
-            <p>Cedula *****${cedula}</p>
-            <p>Telefono ***${telefono}</p>
+          <p><strong>Nombre:</strong> ${nombre}</p>
+          <p><strong>Cédula:</strong> *****${redCedula}</p>
+          <p><strong>Teléfono:</strong> ****${redTelefono}</p>
         </article>
     
         <article class="art2">
-            <p class="p-1"><span>Estimado (a) ${nombre},</span></p>
-            Es un honor dirigirnos a usted <span> ${nombre}</span>
-            Hemos tenido el privilegio de trabajar he informarle que su solicitud para crear una cuenta de Cliente Fiel con
-            <span> Grupo Empresarial Multired (Gane Yumbo)</span> ha sido exitosa.
-            Ahora podras acumular puntos cada vez que compre formularios de Azar (Chance). Seria autorizado en nuestros
-            puntos de veta del Municipio de Yumbo.
+          <p><strong>Estimado(a) ${nombre},</strong></p>
+          <p>Hemos recibido su información suministrada por el ChatBot de la compañoa y tenido el privilegio de trabajar para informarle que su solicitud para crear una cuenta de <span> Cliente Fiel</span> con el <span>Grupo Empresarial Multired</span> (Gane Yumbo) ha sido exitosa. Ahora podrá acumular puntos cada vez que compre formularios de Azar (Chance). en nuestros puntos de venta autorizados del Municipio de Yumbo.</p>
         </article>
     
-        <article class="art3">
-            <span>Atentamente,</span>
-            <br>
-            <br>
-            Grupo Empresarial Multired SA
-            Generado Automaticamente. <span>No responder este correo.</span>
+        <article>
+          <p><strong>Atentamente,</strong></p>
+          <p class="pfoter">Grupo Empresarial Multired SA</p>
+          <p>Correo generado automaticamente <span>No responder este correo</span></p>
         </article>
-    
+      </main>
     </body>
-    
     </html>
     `
   )
