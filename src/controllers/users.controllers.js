65aa448042bbbc4c2a2e9ca90e2a7024bbb7fa76
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-11-17 14:31:52 -0500

    cambios agregados a User.created

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -8,22 +8,29 @@ dotenv.config()
 const JWT_SECRET = process.env.JWT_SECRET
 const BCRYPT_SALT_ROUNDS = 10
 
+if (!JWT_SECRET) {
+  throw new Error('JWT_SECRET no está definida')
+}
+
 export const getUser = async (req, res) => {
   try {
     const token = req.headers.authorization?.split(' ')[1]
     if (!token) {
-      throw new Error('No Token')
+      throw new Error('No se proporcionó un token')
     }
     const userData = jwt.verify(token, JWT_SECRET, { sameSite: 'none', secure: 'true' })
     res.json(userData)
   } catch (error) {
-    res.status(401).json(error.message)
+    res.status(401).json({ message: error.message })
   }
 }
 
 export const getLogin = async (req, res) => {
   try {
     const { user, password } = req.body
+    if (!user || !password) {
+      return res.status(400).json({ error: 'El usuario y la contraseña son requeridos' })
+    }
     const [result] = await conecToLoginMysql.query('SELECT id, nombres, apellidos, correo, username, password, proceso FROM login_chat WHERE username = ?', [user])
     if (result.length === 0) {
       throw new Error('El Usuario no existe ¡')
@@ -34,7 +41,8 @@ export const getLogin = async (req, res) => {
     if (!passwordMatches) {
       throw new Error('Clave inválida verifiquela')
     }
-    const token = jwt.sign({ id, username, nombres, apellidos, correo, proceso }, JWT_SECRET)
+
+    const token = jwt.sign({ id, username, nombres, apellidos, correo, proceso }, JWT_SECRET, { expiresIn: '1h' })
     res.cookie('token', token, { sameSite: 'none', secure: 'true' }).status(200).json({ id, username, nombres, apellidos, correo, proceso, token })
   } catch (error) {
     res.status(401).json({ error: error.message })
@@ -44,10 +52,13 @@ export const getLogin = async (req, res) => {
 export const createUser = async (req, res) => {
   try {
     const { nombres, apellidos, documento, telefono, correo, proceso } = req.body
+
+    if (!nombres || !apellidos || !documento || !telefono || !correo || !proceso) {
+      return res.status(400).json({ error: 'Todos los campos son requeridos' })
+    }
     const [result] = await conecToLoginMysql.query('SELECT * FROM login_chat WHERE documento = ?', [documento])
     if (result.length > 0) {
-      res.status(200).json({ message: 'Usuario Ya Se Encuentra Registrado' })
-      return
+      return res.status(409).json({ message: 'Usuario Ya Se Encuentra Registrado' })
     }
     const username = `CP${documento}`
     const password = `CP${documento.slice(-3)}`
@@ -60,10 +71,10 @@ export const createUser = async (req, res) => {
     if (UserCreado.affectedRows === 1) {
       res.status(201).json({ message: 'Usuario Registrado Correctamente' })
     } else {
-      throw new Error('Usuario No Creado')
+      throw new Error('Error al crear el usuario')
     }
   } catch (error) {
-    res.status(409).json({ error: error.message })
+    res.status(500).json({ error: error.message })
     console.log(error)
   }
 }
@@ -71,27 +82,28 @@ export const createUser = async (req, res) => {
 export const changePassword = async (req, res) => {
   try {
     const { username, oldPassword, newPassword, confirmPassword } = req.body
-    // TODO: Fetch the user
+    if (!username || !oldPassword || !newPassword || !confirmPassword) {
+      return res.status(400).json({ error: 'Todos los campos son requeridos' })
+    }
     const [users] = await conecToLoginMysql.query('SELECT * FROM login_chat WHERE username = ?', [username])
     if (users.length === 0) {
-      throw new Error('Credenciales inválidas')
+      return res.status(401).json({ error: 'Credenciales inválidas' })
     }
     const user = users[0]
-    // TODO: Check the old password
     const passwordMatches = await bcrypt.compare(oldPassword, user.password)
     if (!passwordMatches) {
-      throw new Error('Contraseña Actual No Coincide')
+      return res.status(401).json({ error: 'Contraseña Actual No Coincide' })
     }
-    // TODO: Validate the new password
     if (newPassword !== confirmPassword) {
-      throw new Error('La nueva contraseña no coinciden')
+      return res.status(400).json({ error: 'La nueva contraseña no coinciden' })
     }
-    // TODO: Hash the new password
     const hashedPassword = await bcrypt.hash(newPassword, BCRYPT_SALT_ROUNDS)
-    // TODO: Update the password in the database
-    await conecToLoginMysql.query('UPDATE login_chat SET password = ? WHERE username = ?', [hashedPassword, username])
+    const [updateResult] = await conecToLoginMysql.query('UPDATE login_chat SET password = ? WHERE username = ?', [hashedPassword, username])
+    if (updateResult.affectedRows === 0) {
+      throw new Error('No se pudo actualizar la contraseña')
+    }
     res.status(200).json({ message: 'Contraseña Actualizada Correctamente' })
   } catch (error) {
-    res.status(400).json({ error: error.message })
+    res.status(500).json({ error: error.message })
   }
 }
