4c20cbeff7f9ac8f5101e21f10e98972dfb9bcee
llOrTeGall <IvanOrtega_97@hotmail.com>
2024-01-25 03:02:55 -0500

    end point de items creados correctamente agregado middleware

--- src/connections/mongoDb.js
+++ src/connections/mongoDb.js
@@ -2,7 +2,22 @@ import mongoose from 'mongoose'
 
 export const ConnetMongoDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI)
+    if (mongoose.connection.readyState) {
+      await mongoose.connection.close()
+    }
+    await mongoose.connect(process.env.MONGODB_URI_MULTIRED)
+  } catch (error) {
+    console.log('MongoDB connection error', error)
+    throw new Error('MongoDB connection error')
+  }
+}
+
+export const ConnetMongoServired = async () => {
+  try {
+    if (mongoose.connection.readyState) {
+      await mongoose.connection.close()
+    }
+    await mongoose.connect(process.env.MONGODB_URI_SERVIRED)
   } catch (error) {
     console.log('MongoDB connection error', error)
     throw new Error('MongoDB connection error')
--- src/controllers/Items.Controllers.js
+++ src/controllers/Items.Controllers.js
@@ -1,31 +1,30 @@
 import { ItemModel } from '../Models/Models.js'
-import { ConnetMongoDB } from '../Connections/MongoDb.js'
 
 export const getItems = async (req, res) => {
   try {
-    await ConnetMongoDB()
     const items = await ItemModel.find()
-    res.status(200).json(items)
+    return res.status(200).json(items)
   } catch (error) {
     console.error(error)
-    res.status(500).json({ error: 'Error al obtener los ítems' })
+    res.status(500).send('Error al obtener los items')
   }
 }
 
 export const createItem = async (req, res) => {
+  const { nombre, descripcion, placa, serial, estado, company } = req.body
+
   try {
-    const { nombre, descripcion, placa, serial, estado } = req.body
-
-    if (!/^MI-\d{1,5}$|^MA-\d{1,5}$/.test(placa)) {
-      return res.status(400).json({ error: 'La placa debe comenzar con "MI-" o "MA-" seguido de un número de hasta 5 dígitos' })
-    }
-
     // Validar los datos de entrada
     if (!nombre || !descripcion || !placa || !serial || !estado) {
-      return res.status(400).json({ error: 'Faltan campos requeridos' })
+      throw new Error('Faltan campos requeridos')
     }
 
-    await ConnetMongoDB()
+    const plateRegex = company === 'multired' ? /^MI-\d{1,5}$|^MA-\d{1,5}$/ : /^SI-\d{1,5}$|^SA-\d{1,5}$/
+    const plateError = company === 'multired' ? 'La placa debe comenzar con "MI-" o "MA-" seguido de un número de hasta 5 dígitos' : 'La placa debe comenzar con "SI-" o "SA-" seguido de un número de hasta 5 dígitos'
+
+    if (!plateRegex.test(placa)) {
+      throw new Error(plateError)
+    }
 
     const newItem = new ItemModel({ nombre, descripcion, placa, serial, estado })
     await newItem.save()
@@ -34,78 +33,71 @@ export const createItem = async (req, res) => {
     console.log(error)
 
     if (error.code === 11000) {
-      console.log(error)
       const Code = error.code
       const name = Object.keys(error.keyValue)[0]
       const Value = error.keyValue[Object.keys(error.keyValue)[0]]
-      return res.status(400)
-        .json({ error: `Error: ${Code}, ${name} = ${Value} Ya Existe !!! ` })
+      return res.status(400).json({ error: `Error: ${Code}, ${name} = ${Value} Ya Existe !!! ` })
     }
 
-    res.status(500).json({ error: 'Error al crear el ítem' })
+    res.status(500).json({ error: error.message || 'Error al crear el ítem' })
   }
 }
 
 export const updateItem = async (req, res) => {
+  const { id, descripcion, estado, placa, serial, company } = req.body
+
   try {
-    const { id, descripcion, estado, placa, serial } = req.body
+    // Validar los datos de entrada
+    if (!id || !descripcion || !placa || !serial || !estado) {
+      throw new Error('Faltan campos requeridos')
+    }
 
-    await ConnetMongoDB()
+    const plateRegex = company === 'multired' ? /^MI-\d{1,5}$|^MA-\d{1,5}$/ : /^SI-\d{1,5}$|^SA-\d{1,5}$/
+    const plateError = company === 'multired' ? 'La placa debe comenzar con "MI-" o "MA-" seguido de un número de hasta 5 dígitos' : 'La placa debe comenzar con "SI-" o "SA-" seguido de un número de hasta 5 dígitos'
 
-    if (!/^MI-\d{1,5}$|^MA-\d{1,5}$/.test(placa)) {
-      return res.status(400).json({ error: 'La placa debe comenzar con "MI-" o "MA-" seguido de un número de hasta 5 dígitos' })
+    if (!plateRegex.test(placa)) {
+      throw new Error(plateError)
     }
 
     const item = await ItemModel.findOneAndUpdate({ _id: id }, { descripcion, estado, placa, serial }, { new: true })
 
     if (!item) {
-      return res.status(404).json({ error: 'Item no encontrado' })
+      throw new Error('Item no encontrado')
     }
 
-    return res.status(200).json({ message: 'Item actualizado correctamente' })
-  } catch (error) {
-    if (error.code === 11000) {
-      console.log(error)
-      const name = Object.keys(error.keyValue)[0]
-      const Value = error.keyValue[Object.keys(error.keyValue)[0]]
-      return res.status(400)
-        .json({ error: `${name} = ${Value} Ya Existe !!! ` })
-    }
-
-    return res.status(500).json({ error: 'Error al actualizar el item' })
-  }
-}
-
-export const deleteItem = async (req, res) => {
-  try {
-    const { nombre, descripcion, placa, serial, estado } = req.body
-
-    if (!/^MI-|^MA-/.test(placa)) {
-      return res.status(400).json({ error: 'La placa debe comenzar con "MI-" o "MA-"' })
-    }
-
-    // Validar los datos de entrada
-    if (!nombre || !descripcion || !placa || !serial || !estado) {
-      return res.status(400).json({ error: 'Faltan campos requeridos' })
-    }
-
-    await ConnetMongoDB()
-
-    const newItem = new ItemModel({ nombre, descripcion, placa, serial, estado })
-    await newItem.save()
-    res.status(201).json({ message: 'Ítem creado correctamente' })
+    res.status(200).json({ message: 'Item actualizado correctamente' })
   } catch (error) {
     console.log(error)
 
     if (error.code === 11000) {
-      console.log(error)
       const Code = error.code
       const name = Object.keys(error.keyValue)[0]
       const Value = error.keyValue[Object.keys(error.keyValue)[0]]
-      return res.status(400)
-        .json({ error: `Error: ${Code}, ${name} = ${Value} Ya Existe !!! ` })
+      return res.status(400).json({ error: `Error: ${Code}, ${name} = ${Value} Ya Existe !!! ` })
     }
 
-    res.status(500).json({ error: 'Error al crear el ítem' })
+    res.status(500).json({ error: error.message || 'Error al actualizar el ítem' })
+  }
+}
+
+export const deleteItem = async (req, res) => {
+  const { id, company } = req.params
+
+  try {
+    // Validar los datos de entrada
+    if (!id || !company) {
+      throw new Error('Falta el ID del ítem o la compañía')
+    }
+
+    const item = await ItemModel.findByIdAndDelete(id)
+
+    if (!item) {
+      throw new Error('Ítem no encontrado')
+    }
+
+    res.status(200).json({ message: 'Ítem eliminado correctamente' })
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ error: error.message || 'Error al eliminar el ítem' })
   }
 }
