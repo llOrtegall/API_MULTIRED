2dac05d3e4f38939a730dee65e81ef7ef13a305d
llOrTeGall <IvanOrtega_97@hotmail.com>
2024-01-27 19:36:08 -0500

    cambiamos login chat a login

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -35,7 +35,7 @@ export const Login = async (req, res) => {
     return res.status(200).json(result)
   } catch (error) {
     console.log(error)
-    return res.status(500).json({ message: error.message })
+    return res.status(400).json({ message: error.message })
   }
 }
 
