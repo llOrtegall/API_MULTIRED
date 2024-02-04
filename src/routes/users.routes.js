7bdbe976327dffe34a2ed7beeef72ddeae6e21bc
llOrTeGall <IvanOrtega_97@hotmail.com>
2024-02-04 16:14:54 -0500

    funciona implementar cambios en el app de chat_bot

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,11 +1,9 @@
 import { Router } from 'express'
-import { UserByToken, createUser, Login, changePassword, getUsers, forgotPassword, ResetPassword, LogOutUser } from '../controllers/users.controllers.js'
+import { UserByToken, createUser, Login, changePassword, getUsers, forgotPassword, ResetPassword } from '../controllers/users.controllers.js'
 
 export const LoginUser = Router()
 
 LoginUser.post('/login', Login)
-
-LoginUser.post('/logout', LogOutUser)
 
 LoginUser.get('/profile', UserByToken)
 
