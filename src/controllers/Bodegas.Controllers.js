18ec5d9e4cb2ace1ae6ef20c26a84760f2473302
llOrTeGall <IvanOrtega_97@hotmail.com>
2024-02-22 11:39:04 -0500

    apply changes

--- src/controllers/Bodegas.Controllers.js
+++ src/controllers/Bodegas.Controllers.js
@@ -95,7 +95,7 @@ export const addItemToBodega = async (req, res) => {
     return
   }
 
-  if(itemIds.length === 0) {
+  if (itemIds.length === 0) {
     res.status(400).json({ error: 'Se debe seleccionar mínimo un Ítem.' })
     return
   }
